d4e91d5892f7bcd57e484c1ef77b40e01753ce3a
spoonm <spoonm@spoonm.org>
2016-11-23 22:28:25 -0200

    Changed to bytearray. Ignoring extra closing ].

--- bfck-py/bfck.py
+++ bfck-py/bfck.py
@@ -3,11 +3,11 @@
 import sys
 import time
 
-ins_tape =  ""	# instructions tape
-lop_tape = [ ]	# loop indexes tape
-dat_tape = [0]	# data tape
-dat_indx =  0 	# data index (will be used as pointer)
-ins_indx =  0 	# instruction index
+ins_tape = ""            	# instructions tape
+lop_tape = []            	# loop indexes tape
+dat_tape = bytearray([0])	# data tape
+dat_indx = 0             	# data index (will be used as pointer)
+ins_indx = 0             	# instruction index
 
 out_str = ""  	# output string
 p_sleep = 0.03	# loop sleep
@@ -15,13 +15,19 @@ p_sleep = 0.03	# loop sleep
 # increment/decrement current cell value
 def inc_cell(_):
 
-	dat_tape[dat_indx] += 1
+	try:
+		dat_tape[dat_indx] += 1
+	except ValueError:
+		dat_tape[dat_indx] = 0
 
 	return _ + 1
 
 def dec_cell(_):
 
-	dat_tape[dat_indx] -= 1
+	try:
+		dat_tape[dat_indx] -= 1
+	except ValueError:
+		dat_tape[dat_indx] = 255
 
 	return _ + 1
 
@@ -44,7 +50,7 @@ def dec_pntr(_):
 	if dat_indx != 0:
 		dat_indx -= 1
 	else:
-		raise Exception("Attempt to go to cell #-1")
+		dat_tape.insert(0, 0)
 
 	return _ + 1
 
@@ -57,12 +63,15 @@ def start_loop(indx):
 
 def end_loop(indx):
 
-	if dat_tape[dat_indx] != 0:
-		return lop_tape[-1]
-	else:
-		lop_tape.remove(lop_tape[-1])
+	try:
+		if dat_tape[dat_indx] != 0:
+			return lop_tape[-1]
+		else:
+			lop_tape.remove(lop_tape[-1])
 
-		return indx + 1
+			return indx + 1
+	except IndexError:
+		return indx + 1 # ignore unmatched closing loop
 
 # character io
 def read_char(_):
@@ -122,24 +131,24 @@ while ins_indx < len(ins_tape):
 
 	print("\033[1;1H", end="")
 	print("  Output:", out_str.replace('\n', "\\n"))
-	print("   Cells:", '\t'.join(["{:04d}".format(_) for _ in dat_tape]))
-	print(" Pointer:", "    \t" * dat_indx, "^", " " * 20)
+	print("   Cells:", '\t'.join(["{:03d}".format(_) for _ in dat_tape]))
+	print(" Pointer:", "   \t" * dat_indx, " ^", " " * 20)
 	print("Operator:", op_c)
 
 	# rewind the tape if we're caught by the loop
 	# `and ins_indx != old_indx` takes care of infinite loops
-	if ins_indx != old_indx + 1 and ins_indx != old_indx:
+	if ins_indx < old_indx + 1 and ins_indx != old_indx:
 		while old_indx != ins_indx:
 			old_indx -= 1
 			print("\033[5;1H", end="")
-			print("    Next:", ins_prep(old_indx, 20), "\033[34m" +
-				ins_tape[old_indx] + "\033[0m", ins_tape[old_indx+1:old_indx+21],
+			print("    Next:", ins_prep(old_indx, 35), "\033[34m" +
+				ins_tape[old_indx] + "\033[0m", ins_tape[old_indx+1:old_indx+36],
 				" " * 20)
 
 			time.sleep(p_sleep)
 	else:
-		print("    Next:", ins_prep(ins_indx, 20), "\033[31m" +
+		print("    Next:", ins_prep(ins_indx, 35), "\033[31m" +
 			(ins_tape[ins_indx] if ins_indx < len(ins_tape) else "HALT") + "\033[0m",
-			ins_tape[ins_indx+1:ins_indx+21], " " * 20)
+			ins_tape[ins_indx+1:ins_indx+36], " " * 20)
 
 		time.sleep(p_sleep)
