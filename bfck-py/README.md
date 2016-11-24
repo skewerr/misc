9224fe95a6db7a894808498a0dbcda972cec1b22
spoonm <spoonm@spoonm.org>
2016-11-23 22:48:27 -0200

    Added random program generator. Updated README.md

--- bfck-py/README.md
+++ bfck-py/README.md
@@ -3,6 +3,11 @@
 A [Brainfuck](https://esolangs.org/wiki/brainfuck) interpreter written in
 Python.
 
+# randfile.c
+
+This is a short program that generates random Brainfuck programs/scripts,
+written in C for who knows what reason.
+
 # Issues?
 
 Like [the one I made in C](../bfck-c), many.
---
+++ bfck-py/randfile.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/*
+ * This program will generate random sequences of Brainfuck ops/symbols without
+ * caring for whether it outputs infinite loops, unmatching braces, or anything
+ * that'd cause trouble for a Brainfuck interpreter or render the program
+ * "invalid".
+ *
+ * This is just for fun, please don't be mean.
+ *
+ * It's quite boring to see a bunch of short AND infinite loops go through the
+ * interpreter, so a friend gave me an idea: to set a minimum of operations
+ * inside the loops. That's what the variable `int cnt;` is for.
+ *
+ * I made it start at 0 because I feared random values could interfere with the
+ * first few characters, but that might not be the case and it's a useless
+ * assignment. Meh.
+ */
+
+int
+main(void)
+{
+	const char *c = "+-,.[]<>";
+	int length, cnt = 0;
+	char op;
+
+	srand(time(NULL));
+
+	length = 100 + (rand() % 200);
+
+	for (; length; --length)
+	{
+		op = c[rand() % strlen(c)];
+
+		while (op == ']' && cnd - length < 8)
+			op = c[rand() % strlen(c)];
+
+		if (op == '[')
+			cnt = length;
+
+		putchar(op);
+	}
+
+	return EXIT_SUCCESS;
+}
